82269827e08e62b4020549bc25b5735089928ece
andrezambroni <andrezambroni@gmail.com>
2024-10-22 18:13:06 -0300

    middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,26 @@
 import express from "express"
+import { myMiddleware } from "./middlewares/my-middleware"
 
 const PORT = 3333
 
 const app = express()
+
+app.use(express.json())
+
+// usando middleware global
+app.use(myMiddleware)
+
+app.get("/products", (request, response) => {
+  const { page, limit } = request.query
+
+  response.send(`Página ${page} de ${limit}`)
+})
+
+// middleware local, em uma rota especifica
+app.post("/products", myMiddleware, (request, response) => {
+  const { name, price } = request.body
+
+  response.status(201).json({ name, price })
+})
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
